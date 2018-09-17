8b824397e0c6f3fc10698ac2d9a5f6a85afe8d20
Michael Tien <mtien888@gmail.com>
2018-09-16 20:57:19 -0700

    ADD Echo Test PC program using WINUSB.NET library

---
+++ TEST/WINUSB_EchoTest/WINUSB_EchoTest/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WINUSB_EchoTest
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.MsgBox = new System.Windows.Forms.ListBox();
+         this.SendDataTextBox = new System.Windows.Forms.TextBox();
+         this.button1 = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // MsgBox
+         // 
+         this.MsgBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.MsgBox.FormattingEnabled = true;
+         this.MsgBox.Location = new System.Drawing.Point(425, 7);
+         this.MsgBox.Name = "MsgBox";
+         this.MsgBox.Size = new System.Drawing.Size(489, 446);
+         this.MsgBox.TabIndex = 0;
+         this.MsgBox.SelectedIndexChanged += new System.EventHandler(this.MsgBox_SelectedIndexChanged);
+         // 
+         // SendDataTextBox
+         // 
+         this.SendDataTextBox.Location = new System.Drawing.Point(27, 71);
+         this.SendDataTextBox.Name = "SendDataTextBox";
+         this.SendDataTextBox.Size = new System.Drawing.Size(382, 20);
+         this.SendDataTextBox.TabIndex = 5;
+         this.SendDataTextBox.Text = "30, 31,32, 33, 34, 35, 36, 37, 38, 39, 3A, 3B, 3C, 3D, 3E, 3F";
+         // 
+         // button1
+         // 
+         this.button1.Location = new System.Drawing.Point(27, 111);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(200, 59);
+         this.button1.TabIndex = 6;
+         this.button1.Text = "button1";
+         this.button1.UseVisualStyleBackColor = true;
+         this.button1.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(914, 466);
+         this.Controls.Add(this.button1);
+         this.Controls.Add(this.SendDataTextBox);
+         this.Controls.Add(this.MsgBox);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.ListBox MsgBox;
+      private System.Windows.Forms.TextBox SendDataTextBox;
+      private System.Windows.Forms.Button button1;
+   }
+}
+
---
+++ TEST/WINUSB_EchoTest/WINUSB_EchoTest/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MadWizard.WinUSBNet;
+namespace WINUSB_EchoTest
+{
+   public partial class Form1 : Form
+   {
+      public static USBDevice mWinUSBDevice = null;
+      public static USBInterface mWinUSBInterface = null;
+      public static byte[] readBuffer = new byte[512];
+      public static IAsyncResult readResult;
+      const string DeviceInterfaceGUID = "{13eb360b-bc1e-46cb-ac8b-ef3da47b4062}";
+      public void CreateUSBDevice(String DeviceINterfaceGuid)
+      {
+         AsyncCallback callBack = new AsyncCallback(ProcessWINUSBData);
+         mWinUSBDevice = USBDevice.GetSingleDevice(DeviceINterfaceGuid);
+         if (mWinUSBDevice != null)
+         {
+            mWinUSBInterface = mWinUSBDevice.Interfaces.Find(USBBaseClass.VendorSpecific);
+         }
+         if (mWinUSBInterface != null)
+         {
+            if (mWinUSBInterface != null)
+            {
+               readResult = mWinUSBInterface.InPipe.BeginRead(readBuffer, 0, 512, callBack, this);
+            }
+         }
+      }
+      private void ProcessWINUSBData(IAsyncResult result)
+      {
+         int dataRead = mWinUSBInterface.InPipe.EndRead(result);
+         DataReceived(this, readBuffer, dataRead);
+         // continue to next read
+         AsyncCallback callBack = new AsyncCallback(ProcessWINUSBData);
+         readResult = mWinUSBInterface.InPipe.BeginRead(readBuffer, 0, 512, callBack, this);
+
+      }
+
+      public Form1()
+      {
+         InitializeComponent();
+      }
+      delegate void AddTextCallback(string text);
+      private void AddText(string text)
+      {
+         // InvokeRequired required compares the thread ID of the
+
+         // calling thread to the thread ID of the creating thread.
+
+         // If these threads are different, it returns true.
+
+         if (this.MsgBox.InvokeRequired)
+         {
+            AddTextCallback d = new AddTextCallback(AddText);
+            this.Invoke(d, new object[] { text });
+         }
+         else
+         {
+            this.MsgBox.Items.Add(text);
+            MsgBox.TopIndex = MsgBox.Items.Count - 1;
+         }
+      }
+      private void DataReceived(object sender, byte[] dataReceived, int data_length)
+      {
+         byte[] dataReceived1 = new byte[data_length];
+         Array.Copy(dataReceived, dataReceived1, data_length);
+         string str;
+         if (dataReceived1[0] == '@')
+         {
+            str = "[R]" + System.Text.Encoding.UTF8.GetString(dataReceived1);
+         }
+         else
+            str =
+            "[R]" + BitConverter.ToString(dataReceived1).Replace(',', '-');
+         AddText(str);
+      }
+      private void DataSend(object sender, byte[] dataSent, int data_length)
+      {
+         string str =
+            "[S]" + BitConverter.ToString(dataSent, 0, data_length).Replace(',', '-');
+         AddText(str);
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         if (mWinUSBDevice == null)
+         {
+            MessageBox.Show("WINUSB Device not found");
+            return;
+         }
+         List<int> DataList = SendDataTextBox.Text.Split(',').Select(s => Convert.ToInt32(s.Trim(), 16)).ToList();
+         UInt16 length = (UInt16)DataList.Count;
+         byte[] dataToSend = new byte[length];
+         for (int i = 0; i < DataList.Count; i++)
+            dataToSend[i] = (byte)DataList[i];
+         mWinUSBInterface.OutPipe.Write(dataToSend);
+         DataSend(this, dataToSend, length);
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         CreateUSBDevice(DeviceInterfaceGUID);
+      }
+
+      private void MsgBox_SelectedIndexChanged(object sender, EventArgs e)
+      {
+         MsgBox.Items.Clear();
+      }
+   }
+}
